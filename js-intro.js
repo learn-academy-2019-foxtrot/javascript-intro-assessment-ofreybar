17c6367117f6f2fdb4a9e31c560b169a7da21fbb
Ubuntu <ubuntu@ip-172-31-47-206.us-east-2.compute.internal>
2019-11-26 16:33:47 +0000

    Completed Assessment

--- js-intro.js
+++ js-intro.js
@@ -3,19 +3,20 @@
 
 // --------------------------- Consider the following variable:
 
-// var mantra = "Be the dev"
+var mantra = "Be the dev"
 
 // 1a. Write the code that determines if there is a 'B' in mantra.
 
-// console.log(mantra.includes("B"))
+console.log(mantra.includes("B"))
+// console.log(mantra.match(/B/))
 
 // 1b. Write the code that determines if there is an 'x' in mantra.
 
-// console.log(mantra.includes("x"))
+console.log(mantra.includes("x"))
 
 // 1c. Write the code that determines if there is a 'v' in mantra.
 
-// console.log(mantra.includes("v"))
+console.log(mantra.includes("v"))
 
 // 1d. STRETCH: Write a function that can take two arguments - a string and a letter and returns a sentence telling the user if the letter exists within the string.
 
@@ -23,35 +24,38 @@
 // var str2 = "e"
 // console.log(str1.concat(str2))
 
-// var myName = ("apple" || "q") 
-// if (myName.includes("e")){
-//     console.log("The letter exists in the string!")
-// } else {
-//     console.log("the letter does NOT exist in the string!")
-// }
+var myName = ("apple" || "q") 
+if (myName.includes("e")){
+    console.log("The letter exists in the string!")
+} else {
+    console.log("the letter does NOT exist in the string!")
+}
 
 // ------------------------------ Consider the following variables:
 
-// var myDog = "Snoopy"
-// var myCat = "Garfield"
+var myDog = "Snoopy"
+var myCat = "Garfield"
 
 // 2a. Create a function that returns the longer of the two variables. Expected output: "Garfield"
 
-// if (myDog.length < myCat.length){
-//     console.log(myCat)
-// } else {
-//     console.log(myDog)
-// }
+var longerWord = () =>{
+  if (myDog.length < myCat.length){
+    return "Garfield"
+  } else {
+    return "Snoopy"
+  }
+}
+console.log(longerWord());
 
 // 2b. Create a function that takes in two arguments and returns an array containing the two variables. Expected output: ["Snoopy", "Garfield"]
 
-// var arr = [myDog, myCat]
-// console.log(arr)
+var arr = [myDog, myCat]
+console.log(arr)
 
 // 2c. STRETCH: Write a function that accepts two variables and returns a string in all lowercase letters. Expected output: "snoopy garfield"
 
-// let name = (myDog + " " + myCat)
-// console.log(name.toLowerCase())
+let name = (myDog + " " + myCat)
+console.log(name.toLowerCase())
 
 // ------------------------------ Consider the following variable:
 
@@ -60,56 +64,61 @@
 // 3a. Write the code that logs each letter of the message using a for loop.
 //hint: "logs" in this case means "prints to the terminal console."  This is typically done with a console.log(x), where x is the string, function, etc that you wish to see in the terminal.
 
-// var myMessage = "Learning is fun!";
-// for (var i = 0; i < myMessage.length; i++)
-// console.log(myMessage.charAt(i))
+var myMessage = "Learning is fun!";
+for (var i = 0; i < myMessage.length; i++)
+console.log(myMessage.charAt(i))
 
 // 3b. Write the code that logs each letter of the message using map.
 
 // var myMessage = "Learning is fun!";
-// var letters = myMessage.split("")
-// letters.map(value => value.letters)
-// console.log(letters)
+var letters = myMessage.split("")
+letters.map(value => value.letters)
+console.log(letters)
 
 
 // 3c. STRETCH: Write the code that logs each letter of the message using a while loop.
 
 // var myMessage = "Learning is fun!";
-// var i = 0
-// while (i < myMessage.length) {
-//     console.log(myMessage[i]);
-//     i++
-// }
+var i = 0
+while (i < myMessage.length) {
+    console.log(myMessage[i]);
+    i++
+}
 
 
 // 3d. SUPER STRETCH: Write the code that logs each letter of the message using forEach().
 
 // var myMessage = "Learning is fun!"
-// // var letters = myMessage.split("").forEach("") 
-// myMessage.forEach("")
-// function letters(item){
-//     letters += item
-// }
-// console.log(myMessage)
 
+myMessage.split("").forEach((letter) => {
+  console.log(letter);
+})
 
 
 // ---------------------------- Consider the following variable:
 
-// var testString = "thisisateststring"
+var testString = "thisisateststring"
 
 // 4a. Write a function that takes the variable as an argument and returns the string without vowels. Expected output: "thsstststrng"
 
 // var testString = "thisisateststring"
-// var vowRemover = testString.split("").filter((letter) =>
-// "aeiou".includes(letter.toLowerCase()) === false).join("")
-// console.log(vowRemover)
+var vowRemover = testString.split("").filter((letter) =>
+"aeiou".includes(letter.toLowerCase()) === false).join("")
+console.log(vowRemover)
 
 
 // 4b. STRETCH: Update your function to return an error message if the variable is not a string. Copy and paste your function to add these changes.
 
+// const devowelifier = (string) = {
+//     if(typeof string != "string"){
+//         raise "Booom!"
+//     } else {
+        
+//     }
+// }
 
-
+// console.log(devowelifier(222))
+// console.log(testString)
 
 
 
@@ -119,45 +128,28 @@
 
 // 5a. Write a function that takes in the toonimals array as an argument and returns an array with only the toon objects that are cats. Expected output: [ { name: "Stimpy", animal: "cat" }, { name: "Scratchy", animal: "cat" }, { name: "Felix", animal: "cat" } ]
 
-// var toonimals = [ 
-//     {name: "Itchy", animal: "mouse"}, 
-//     {name: "Stimpy", animal: "cat"}, 
-//     {name: "Daffy", animal: "duck"}, 
-//     {name: "Scratchy", animal: "cat"}, 
-//     {name: "Ren", animal: "dog"}, 
-//     {name: "Felix", animal: "cat"},
-//     ]
+var toonimals = [ 
+    {name: "Itchy", animal: "mouse"}, 
+    {name: "Stimpy", animal: "cat"}, 
+    {name: "Daffy", animal: "duck"}, 
+    {name: "Scratchy", animal: "cat"}, 
+    {name: "Ren", animal: "dog"}, 
+    {name: "Felix", animal: "cat"},
+    ]
 
-// var animal = toonimals.filter(value => value.animal === "cat")
-// console.log(animal);
+var animal = toonimals.filter(value => value.animal === "cat")
+console.log(animal);
 
 
-//5b. Write a function that returns only the names of all the non-cats. Expected output: "Itchy" "Daffy" "Ren"
 
-// var toonimals = [ 
-//     {name: "Itchy", animal: "mouse"}, 
-//     {name: "Stimpy", animal: "cat"}, 
-//     {name: "Daffy", animal: "duck"}, 
-//     {name: "Scratchy", animal: "cat"}, 
-//     {name: "Ren", animal: "dog"}, 
-//     {name: "Felix", animal: "cat"},
-//     ]
+var noCats = toonimals.filter(value => value.animal !== "cat").map(value => value.name) 
+console.log(noCats)
 
-// var noCats = toonimals.filter(value => value.animal !== "cat").map(value => value.name) 
-// console.log(noCats)
 
 //5c. STRETCH: Create a function that will print a sentence for every toon stating their name and their animal type. Use string interpolation. Expected output: "Itchy is a mouse" "Stimpy is a cat" "Daffy is a duck" "Scratchy is a cat" "Ren is a dog" "Felix is a cat"
 
-// var toonimals = [ 
-//     {name: "Itchy", animal: "mouse"}, 
-//     {name: "Stimpy", animal: "cat"}, 
-//     {name: "Daffy", animal: "duck"}, 
-//     {name: "Scratchy", animal: "cat"}, 
-//     {name: "Ren", animal: "dog"}, 
-//     {name: "Felix", animal: "cat"},
-//     ]
     
-// const describeAnimal = (array) =>{
-//     return array.map(value => `${value.name} is a ${value.animal}`).join("\n")
-// }
-// console.log(describeAnimal(toonimals))
+const describeAnimal = (array) =>{
+    return array.map(value => `${value.name} is a ${value.animal}`).join("\n")
+}
+console.log(describeAnimal(toonimals))
--- js-intro.md
+++ js-intro.md
@@ -5,49 +5,48 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. What is a function? Why would you use one?
 
-  Your answer:
+  Your answer: Calls upon an object at any time. (can be associated with an object(method))
 
-  Researched answer:
-
+  Researched answer: Are subprograms that are objects and return a value when called upon.
 
 
 2. List 5 examples of JavaScript array methods.
 
-  Your answer:
+  Your answer: .push, .split, .join, .shift, .unshift, 
 
-  Researched answer:
-
+  Researched answer: .splice, .pop, .push, .shift, .unshift, .concat, .reverse, .map, .filter, .toLowerCase, .toUpperCase, 
 
 
 3. Explain the difference between console.log() and return.
 
-  Your answer:
+  Your answer: console.log() pushes an answer to the log in terminal while return will push if an answer is a specific answer.
 
-  Researched answer:
-
+  Researched answer: console.log is really good for debugging in the middle of your code, while return allows a function to output a value when called upon.
 
 
 4. In regards to functions, what is an argument? What is the difference between an argument and a variable?
 
-  Your answer:
+  Your answer: What you put in the () to tell the function what it's working with. Variable is defined whereas an arguement is not always defined.
 
-  Researched answer:
-
+  Researched answer: An arguement is an object that contains values that is passed to a function. Variables are listed as a part of the function
+  where as arguements are pushed into the function when called upon.
 
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-  Your answer:
+  Your answer: There is a driver and a navigator. One codes while the other helps by thinking of possible solutions and fixing syntax issues.
 
-  Researched answer:
-
+  Researched answer: A software development techniques where two people work in a single workstation frequently changing positions as driver and navigator/observer.
 
 
 6. Give a brief description of JavaScript as if you were explaining it to someone who doesn't know anything about programming.
 
-  Your answer:
+  Your answer: It is a backend language that helps design a user friendly/client sided code that can be interacted by a a user. Javascrips is usually 
+  used for building webpages.
 
 
 7. What is one thing we did in class this week you found helpful?  
 
-  Your answer:
+  Your answer: Classes. Being able to define a class and keep pulling the code you used earlier to newer classes so that you don't have to keep 
+  re writing the code was very helpful and I can see it being used in the future. It was also a lot easier to understand over some of the 
+  other topics we covered during the first week of coding. 
